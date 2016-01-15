14016f0f61d720068f49c26282defdb8215752a0
Ajoe <sdec0817@gmail.com>
2016-01-15 15:21:17 +0800

    Add Project
    複製 1komo

---
+++ Work.WebProj/webpack.config.js
@@ -0,0 +1,43 @@
+﻿var path = require('path');
+var webpack = require('webpack');
+//var node_modules_dir = path.resolve(__dirname, 'node_modules');
+module.exports = {
+    entry: {
+        //基礎功能
+        m_menu: path.resolve(__dirname, 'Scripts/src/tsx/m-menu.js'),
+        m_login: path.resolve(__dirname, 'Scripts/src/tsx/m-login.js'),
+        m_roles: path.resolve(__dirname, 'Scripts/src/tsx/m-roles.js'),
+        m_change_password: path.resolve(__dirname, 'Scripts/src/tsx/m-change_password.js'),
+        m_users: path.resolve(__dirname, 'Scripts/src/tsx/m-users.js'),
+        //後台 管理者
+        m_sales: path.resolve(__dirname, 'Scripts/src/tsx/m-sales.js'),
+        m_news: path.resolve(__dirname, 'Scripts/src/tsx/m-news.js'),
+        m_aboutus: path.resolve(__dirname, 'Scripts/src/tsx/m-aboutus.js'),
+        m_parm: path.resolve(__dirname, 'Scripts/src/tsx/m-parm.js'),
+        m_banner: path.resolve(__dirname, 'Scripts/src/tsx/m-banner.js'),
+
+        vendors: ['jquery', 'react', 'react-bootstrap', 'moment']
+    },
+    output: {
+        path: path.resolve(__dirname, 'Scripts/build/app'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+          { test: /\.jsx$/, loader: 'babel' },
+          { test: /\.css$/, loader: "style-loader!css-loader" }
+        ]
+    },
+    resolve: {
+        alias: {
+            moment: "moment/moment.js"
+        },
+        modulesDirectories: ["app_modules", "node_modules"],
+        extensions: ['', '.js', 'jsx', '.json']
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+      new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /zh-tw/),
+      new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
+    ]
+};
