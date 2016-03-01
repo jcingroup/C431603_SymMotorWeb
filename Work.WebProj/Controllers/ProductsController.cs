44dbc8fe81877b0832d59336ebefff342bdeefee
lei-chieh <lei.chieh@gmail.com>
2016-03-01 15:02:55 +0800

    CSS (行動版)、+車款展示

---
+++ Work.WebProj/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using DotWeb.Controller;
+using System.Collections.Generic;
+using ProcCore.Business.DB0;
+using System.Linq;
+
+namespace DotWeb.Controllers
+{
+    public class ProductsController : WebUserController
+    {
+        public ActionResult Index()
+        {
+            return View("Products");
+        }
+    }
+}
