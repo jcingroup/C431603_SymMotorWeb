58f9276608c189df69a5ebeeb5dd67b47d7ab28b
Ajoe <sdec0817@gmail.com>
2016-03-11 12:15:49 +0800

    地圖加入自動定位

--- Work.WebProj/Content/js/gmap-custom.js
+++ Work.WebProj/Content/js/gmap-custom.js
@@ -63,6 +63,24 @@ function init() {
         });
         markers.push(marker);
     }
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('±zªº¦ì¸m');
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
 function setNewMapMarker(type) {
     for (var i = 0; i < markers.length; i++) {
--- Work.WebProj/Content/js/gmap-location.js
+++ Work.WebProj/Content/js/gmap-location.js
@@ -73,6 +73,24 @@ function init() {
         });
         markers.push(marker);
     }
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('您的位置');
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
 
 function setNewMapMarker(type) {
