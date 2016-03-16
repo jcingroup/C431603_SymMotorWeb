82049ef3b2c2027b3734fc4bd80ada0a5b7f2ea7
lei-chieh <lei.chieh@gmail.com>
2016-03-16 12:21:34 +0800

    3/14 修改

---
+++ Work.WebProj/Controllers/MapController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using DotWeb.Controller;
+using ProcCore.Business.DB0;
+using System.Linq;
+
+namespace DotWeb.Controllers
+{
+    public class MapController : WebUserController
+    {
+        public ActionResult Index()
+        {
+            return View("Map");
+        }
+    }
+}
