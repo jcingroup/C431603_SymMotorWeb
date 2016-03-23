f03886a0bd01e35702d85457e68cb328d27f3923
Jerry Fu <fusun@ms7.hinet.net>
2016-03-23 20:17:18 +0800

    set search field

--- Work.WebProj/Scripts/src/jsx/w-usedcar-list.jsx
+++ Work.WebProj/Scripts/src/jsx/w-usedcar-list.jsx
@@ -4,13 +4,27 @@ var moment = require('moment');
 
 var ListCars = React.createClass({
     getInitialState: function () {
+
+        var now = new Date();
+        var opt_year = [];
+        for(var i=0; i < 5; i++ ) {
+            opt_year.push(now.getFullYear()-i);
+        }
+
         return {
             lists:[],
             search:{
-                h_obj_brand:null
+                h_obj_brand:null,
+                h_obj_type:null,
+                h_obj_color:null,
+                h_obj_born_date:null,
+                h_place_dept_no:null,
+                h_list_price:null
             },
             options_brand:[],
-            options_modal:[]
+            options_modal:[],
+            options_year:opt_year,
+            options_place:[]
         };
     },
     getDefaultProps: function () {
@@ -30,6 +44,10 @@ var ListCars = React.createClass({
             this.setState({options_brand:data});
         }.bind(this));
 
+        $.get(this.props.sym_web_api + 'api_place_dept_no.asp',{})
+        .done(function(data){
+            this.setState({options_place:data});
+        }.bind(this));
     },
     setSearchField:function(n,e){
         var obj = this.state.search;
@@ -65,17 +83,20 @@ var ListCars = React.createClass({
                                         onChange={this.onChangeBrand}>
                                     <option value="">廠牌</option>
                                     {
-                                        this.state.options_brand.map(function(item,i){
-                                            return (
+                                    this.state.options_brand.map(function(item,i){
+                                    return (
                                             <option value={item.value} key={item.value}>{item.name}</option>);
-                                        })
+                                    })
                                     }
                                 </select>
                             </div>
                             <div className="form-group">
                                 <label for="">車型</label> {}
-                                <select id="h_obj_type" onChange={this.setSearchField.bind(this,'h_obj_type')} className="form-control c-select style2">
-                                    <option value="" selected disabled>車型</option>
+                                <select id="h_obj_type"
+                                        value={this.state.search.h_obj_type}
+                                        onChange={this.setSearchField.bind(this,'h_obj_type')}
+                                        className="form-control c-select style2">
+                                    <option value="">車型</option>
                                     {
                                         this.state.options_modal.map(function(item,i){
                                             return (
@@ -86,30 +107,67 @@ var ListCars = React.createClass({
                             </div>
                             <div className="form-group">
                                 <label for="">車色</label> {}
-                                <select name="" id="" className="form-control c-select style2">
-                                    <option value="" selected disabled>車色</option>
+                                <select id="h_obj_color"
+                                        value={this.state.search.h_obj_color}
+                                        className="form-control c-select style2"
+                                        onChange={this.setSearchField.bind(this,'h_obj_color')}>
+                                    <option value="">車色</option>
+                                    <option value='黑'>黑</option>
+                                    <option value='白'>白</option>
+                                    <option value='紅'>紅</option>
+                                    <option value='橙'>橙</option>
+                                    <option value='黃'>黃</option>
+                                    <option value='綠'>綠</option>
+                                    <option value='藍'>藍</option>
+                                    <option value='靛'>靛</option>
+                                    <option value='紫'>紫</option>
+                                    <option value='金'>金</option>
+                                    <option value='銀'>銀</option>
+                                    <option value='灰'>灰</option>
+                                    <option value='棕'>棕</option>
+                                    <option value='粉紅'>粉紅</option>
+                                    <option value='咖啡'>咖啡</option>
                                 </select> {}
                             </div>
                             <div className="form-group">
                                 <label for="">年份</label> {}
-                                <select name="" id="" className="form-control c-select style2">
-                                    <option value="" selected disabled>年份</option>
+                                <select id="h_obj_born_date"
+                                        value={this.state.search.h_obj_born_date}
+                                        className="form-control c-select style2"
+                                        onChange={this.setSearchField.bind(this,'h_obj_born_date')}
+                                        >
+                                    <option value="">年份</option>
+                                    {
+            this.state.options_year.map(function(item,i){
+                return (
+                    <option value={item} key={item}>{item}</option>);
+            })
+                                    }
+
                                 </select> {}
                             </div>
                             <div className="form-group">
                                 <label for="">地點</label> {}
-                                <select name="" id="" className="form-control c-select style2">
-                                    <option value="" selected disabled>地點</option>
+                                <select id="h_place_dept_no" 
+                                        value={this.state.search.h_place_dept_no}
+                                        className="form-control c-select style2">
+                                    <option value="">地點</option>
+                                    {
+                                        this.state.options_place.map(function(item,i){
+                                        return (
+                                                <option value={item.value} key={item.value}>{item.name}</option>);
+                                        })
+                                    }
                                 </select> {}
                             </div>
                             <div className="form-group">
                                 <label for="">價格</label> {}
                                 <select name="" id="" className="form-control c-select style2">
-                                    <option value="" selected>$0</option>
+                                    <option value="">$0</option>
                                 </select> {}
                                 <label for="">~</label> {}
                                 <select name="" id="" className="form-control c-select style2">
-                                    <option value="" selected>不限</option>
+                                    <option value="">不限</option>
                                     <option value="">$1,000,000</option>
                                 </select> {}
                             </div>
